18dbd5bf57cc39dfb130f31a0094a3ad5902b279
Ralph Ramos <ralphmtk@gmail.com>
2025-09-04 08:36:38 -0300

    feat(teams): implement CRUD operations for teams with error handling and response structure

--- src/controllers/teams-controllers.ts
+++ src/controllers/teams-controllers.ts
@@ -12,13 +12,78 @@ class TeamController {
 
     const { name, description } = bodySchema.parse(request.body);
 
-    return response.json({ data: { name, description } });
+    const teamAlreadyExists = await prisma.team.findFirst({
+      where: { name },
+    });
+
+    if (teamAlreadyExists) {
+      throw new AppError("Team already exists.", 409);
+    }
+
+    const team = await prisma.team.create({
+      data: {
+        name,
+        description,
+      },
+    });
+
+    return response.json({ data: team });
   }
 
-  async index(request: Request, response: Response) {}
-  async show(request: Request, response: Response) {}
-  async update(request: Request, response: Response) {}
-  async delete(request: Request, response: Response) {}
+  async index(request: Request, response: Response) {
+    const teams = await prisma.team.findMany();
+
+    return response.json({ data: teams });
+  }
+  async show(request: Request, response: Response) {
+    const paramsSchema = z.object({
+      id: z.coerce.number().int().positive(),
+    });
+
+    const { id } = paramsSchema.parse(request.params);
+
+    const team = await prisma.team.findUnique({
+      where: { id },
+    });
+
+    return response.json({ data: team });
+  }
+  async update(request: Request, response: Response) {
+    const paramsSchema = z.object({
+      id: z.coerce.number().int().positive(),
+    });
+
+    const { id } = paramsSchema.parse(request.params);
+
+    const bodySchema = z.object({
+      name: z.string().trim().min(2),
+      description: z.string().trim().min(2),
+    });
+
+    const { name, description } = bodySchema.parse(request.body);
+
+    const team = await prisma.team.update({
+      where: { id },
+      data: {
+        name,
+        description,
+      },
+    });
+
+    return response.json({ data: team });
+  }
+  async delete(request: Request, response: Response) {
+    const paramsSchema = z.object({
+      id: z.coerce.number().int().positive(),
+    });
+    const { id } = paramsSchema.parse(request.params);
+
+    await prisma.team.delete({
+      where: { id },
+    });
+
+    return response.json({ message: "Team deleted successfully." });
+  }
 }
 
 export { TeamController };
--- src/routes/teams-routers.ts
+++ src/routes/teams-routers.ts
@@ -7,7 +7,7 @@ const teamController = new TeamController();
 
 teamsRoutes.post("/", ensureAuthenticated, teamController.create);
 teamsRoutes.get("/", ensureAuthenticated, teamController.index);
-teamsRoutes.get("/:id", ensureAuthenticated, teamController.show);
+teamsRoutes.get("/:id/show", ensureAuthenticated, teamController.show);
 teamsRoutes.put("/:id", ensureAuthenticated, teamController.update);
 teamsRoutes.delete("/:id", ensureAuthenticated, teamController.delete);
 
