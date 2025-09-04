0ff1d3d8cf668a51b0381231c0fc9ce3616ee850
Ralph Ramos <ralphmtk@gmail.com>
2025-09-04 10:23:29 -0300

    feat(teams): add member management functionality with add, remove, and list members endpoints

--- src/controllers/teams-controllers.ts
+++ src/controllers/teams-controllers.ts
@@ -2,6 +2,7 @@ import type { Request, Response } from "express";
 import { AppError } from "@/utils/AppError";
 import { prisma } from "@/database/prisma";
 import { z } from "zod";
+import { id } from "zod/v4/locales/index.cjs";
 
 class TeamController {
   async create(request: Request, response: Response) {
@@ -82,7 +83,98 @@ class TeamController {
       where: { id },
     });
 
-    return response.json({ message: "Team deleted successfully." });
+    return response.json({
+      message: `Team has been ${id} deleted successfully.`,
+    });
+  }
+
+  async addMember(request: Request, response: Response) {
+    const paramsSchema = z.object({
+      id: z.coerce.number().int().positive(),
+    });
+
+    const { id } = paramsSchema.parse(request.params);
+
+    const team = await prisma.team.findUnique({
+      where: { id },
+    });
+
+    const bodySchema = z.object({
+      userId: z.number().int().positive(),
+    });
+
+    const { userId } = bodySchema.parse(request.body);
+
+    const user = await prisma.user.findUnique({
+      where: { id: userId },
+    });
+
+    const memberAlreadyExists = await prisma.teamMember.findFirst({
+      where: {
+        teamId: id,
+        userId,
+      },
+    });
+
+    if (!user || !team) {
+      throw new AppError("User or team not found.", 404);
+    }
+
+    if (memberAlreadyExists) {
+      throw new AppError("User is already a member of the team.", 409);
+    }
+
+    const teamMember = await prisma.teamMember.create({
+      data: {
+        teamId: id,
+        userId,
+      },
+    });
+
+    return response.json({ data: teamMember });
+  }
+
+  async removeMember(request: Request, response: Response) {
+    const paramsSchema = z.object({
+      id: z.coerce.number().int().positive(),
+      userId: z.coerce.number().int().positive(),
+    });
+
+    const { id, userId } = paramsSchema.parse(request.params);
+
+    const teamMember = await prisma.teamMember.findFirst({
+      where: {
+        teamId: id,
+        userId,
+      },
+    });
+
+    if (!teamMember) {
+      throw new AppError("Team member not found.", 404);
+    }
+
+    await prisma.teamMember.delete({
+      where: { id: teamMember.id },
+    });
+
+    return response.json();
+  }
+
+  async listMembers(request: Request, response: Response) {
+    const paramsSchema = z.object({
+      id: z.coerce.number().int().positive(),
+    });
+    const { id } = paramsSchema.parse(request.params);
+
+    const teamMembers = await prisma.teamMember.findMany({
+      where: { teamId: id },
+      include: {
+        team: { select: { name: true } },
+        user: { select: { name: true } },
+      },
+    });
+
+    return response.json({ data: teamMembers });
   }
 }
 
--- src/routes/teams-routers.ts
+++ src/routes/teams-routers.ts
@@ -10,5 +10,12 @@ teamsRoutes.get("/", ensureAuthenticated, teamController.index);
 teamsRoutes.get("/:id/show", ensureAuthenticated, teamController.show);
 teamsRoutes.put("/:id", ensureAuthenticated, teamController.update);
 teamsRoutes.delete("/:id", ensureAuthenticated, teamController.delete);
+teamsRoutes.post("/:id/members", ensureAuthenticated, teamController.addMember);
+teamsRoutes.delete(
+  "/:id/members/:userId",
+  ensureAuthenticated,
+  teamController.removeMember
+);
+teamsRoutes.get("/:id/members", ensureAuthenticated, teamController.listMembers);
 
 export { teamsRoutes };
